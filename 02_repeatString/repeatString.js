c17d96adcb0205a8e64cc2dbe470db26a45abe70
mog-rgb <moughiln@terpmail.umd.edu>
2024-05-28 11:11:40 -0400

    Completed the repeatString exercises All test cases were passed in the repeatString exercise.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
 
+
+const repeatString = function(string, num) {
+
+    let maxNum = num;
+    let returnString = "";
+
+    for (maxNum; maxNum > 0; maxNum-- ) {
+        returnString += string;
+    }
+
+    if (num < 0) {
+        returnString = 'ERROR';
+    }
+
+    return returnString;
 };
 
 // Do not edit below this line
